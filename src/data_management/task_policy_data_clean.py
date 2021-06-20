4ef1f081b9b888d3c7090dac41f760d3226ba759
YuxinWang2020 <yuxinwang9712@gmail.com>
2021-06-20 20:08:53 +0800

    run pre-commit

--- src/data_management/task_policy_data_clean.py
+++ src/data_management/task_policy_data_clean.py
@@ -10,7 +10,11 @@ from src.config import SRC
 def task_OxCGRT_policy_stringency(depends_on, produces):
     # policy stringency index
     policy_data = pd.read_csv(depends_on).query("CountryCode=='NLD'")
-    policy_data['Date'] = pd.to_datetime(policy_data['Date'], format='%Y%m%d', errors='ignore')
-    policy_data = policy_data.set_index('Date')
-    policy_stringency = policy_data[['C1_School closing', 'C2_Workplace closing', 'C6_Stay at home requirements']]
+    policy_data["Date"] = pd.to_datetime(
+        policy_data["Date"], format="%Y%m%d", errors="ignore"
+    )
+    policy_data = policy_data.set_index("Date")
+    policy_stringency = policy_data[
+        ["C1_School closing", "C2_Workplace closing", "C6_Stay at home requirements"]
+    ]
     policy_stringency.to_pickle(produces)
--- src/model_code/format_result.py
+++ src/model_code/format_result.py
@@ -1,34 +1,46 @@
 import pandas as pd
 from statsmodels.iolib.summary2 import summary_col
-from functools import reduce
+
 
 def summary_to_stata_format(summary):
     summary = pd.DataFrame(summary.tables[1].data)
-    summary = summary.set_axis(summary.iloc[0].astype(str), axis='columns').set_index('').iloc[1:, [0, 1, 3]]
+    summary = (
+        summary.set_axis(summary.iloc[0].astype(str), axis="columns")
+        .set_index("")
+        .iloc[1:, [0, 1, 3]]
+    )
     for i in range(len(summary)):
         if float(summary.iloc[i, 2]) < 0.01:
-            summary.iloc[i, 0] = summary.iloc[i, 0].lstrip() + '***'
+            summary.iloc[i, 0] = summary.iloc[i, 0].lstrip() + "***"
         elif float(summary.iloc[i, 2]) < 0.05:
-            summary.iloc[i, 0] = summary.iloc[i, 0].lstrip() + '**'
+            summary.iloc[i, 0] = summary.iloc[i, 0].lstrip() + "**"
         elif float(summary.iloc[i, 2]) < 0.1:
-            summary.iloc[i, 0] = summary.iloc[i, 0].lstrip() + '*'
+            summary.iloc[i, 0] = summary.iloc[i, 0].lstrip() + "*"
         else:
             summary.iloc[i, 0] = summary.iloc[i, 0].lstrip()
-        summary.iloc[i, 1] = '(' + summary.iloc[i, 1].lstrip() + ')'
-    summary = summary.drop(columns='P>|z|').stack()
-    summary = summary.reset_index(level=1,drop=True)
+        summary.iloc[i, 1] = "(" + summary.iloc[i, 1].lstrip() + ")"
+    summary = summary.drop(columns="P>|z|").stack()
+    summary = summary.reset_index(level=1, drop=True)
     return summary
 
+
 def sm_results_format(results, model_names):
-    return summary_col(results, float_format='%.3f', model_names=model_names, stars=True,
-                       info_dict={'': lambda x: '',
-                                  '': lambda x: '',
-                                  'Observation': lambda x: str(int(x.nobs)),
-                                  'R-squared': lambda x: f'{x.prsquared:.5f}',
-                                  }
-                       )
+    return summary_col(
+        results,
+        float_format="%.3f",
+        model_names=model_names,
+        stars=True,
+        info_dict={
+            "": lambda x: "",
+            "Observation": lambda x: str(int(x.nobs)),
+            "R-squared": lambda x: f"{x.prsquared:.5f}",
+        },
+    )
+
 
 def odds_radio_format(odds_radios, model_names):
-    return pd.concat([odds_radio['Odds Ratio'] for odds_radio in odds_radios], axis='columns', join='outer').set_axis(model_names, axis='columns')
-
-
+    return pd.concat(
+        [odds_radio["Odds Ratio"] for odds_radio in odds_radios],
+        axis="columns",
+        join="outer",
+    ).set_axis(model_names, axis="columns")
