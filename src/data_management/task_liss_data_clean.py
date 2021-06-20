fcda3cb843e5713acc53b9189e0208d95816593e
YuxinWang2020 <yuxinwang9712@gmail.com>
2021-06-20 19:45:19 +0800

    blackground data add income

--- src/data_management/task_liss_data_clean.py
+++ src/data_management/task_liss_data_clean.py
@@ -6,31 +6,69 @@ from src.config import BLD
 from src.config import SRC
 
 
-@pytask.mark.depends_on(SRC / "original_data" / "liss" / "background_data_merged.pickle")
+@pytask.mark.depends_on(
+    SRC / "original_data" / "liss" / "background_data_merged.pickle"
+)
 @pytask.mark.produces(BLD / "data" / "liss" / "background.pickle")
 def task_clean_background_data(depends_on, produces):
 
     background = pd.read_pickle(depends_on)
-    select_background = background[["hh_id", "age", "female", "edu", "net_income", "hh_members",
-                                    "hh_children", "location_urban"]].dropna(axis=0, how='any')
-    select_background["age_cut"] = pd.cut(select_background.age,
-                                          [15, 25, 50, 75, 120],
-                                          right=False
-                                          )
-    select_background["net_income_cat"] = pd.cut(select_background['net_income'],
-                                                 [0] + list(range(1, 5000, 1000)) + [5001, 7500, np.inf],
-                                                 right=False)
-    select_background['living_alone'] = (select_background['hh_members'] == 1).astype(
-        int)  # alone dummy 1: alone, 0: not alone
-    select_background['living_with_children'] = (select_background['hh_children'] > 0).astype(
-        int)  # 1: has children, 0: no children
-    select_background['age'] = select_background['age'].astype('int')
-    select_background['hh_id'] = select_background['hh_id'].astype('int')
-    select_background['female'] = select_background['female'].astype('int')
-    select_background['net_income_index'] = select_background['net_income_cat'].cat.codes
-    select_background['net_income'] = select_background['net_income'].clip(0, 6000)
-    select_background['log_net_income'] = np.log(select_background['net_income'].clip(0.001, 6000))
-    select_background['male'] = 1 - select_background['female']
+    select_background = background[
+        [
+            "hh_id",
+            "age",
+            "female",
+            "edu",
+            "net_income",
+            "hh_members",
+            "hh_children",
+            "net_income_hh_eqv",
+            "income_hh_group",
+            "net_income_hh",
+            "gross_income_hh",
+        ]
+    ].dropna(axis=0, how="any")
+    select_background["age_cut"] = pd.cut(
+        select_background.age, [15, 25, 50, 75, 120], right=False
+    )
+    select_background["living_alone"] = (select_background["hh_members"] == 1).astype(
+        int
+    )  # alone dummy 1: alone, 0: not alone
+    select_background["living_with_children"] = (
+        select_background["hh_children"] > 0
+    ).astype(
+        int
+    )  # 1: has children, 0: no children
+    select_background["age"] = select_background["age"].astype("int")
+    select_background["hh_id"] = select_background["hh_id"].astype("int")
+    select_background["female"] = select_background["female"].astype("int")
+    select_background["log_income_hh"] = np.log(
+        select_background["net_income_hh_eqv"].clip(lower=0.001)
+    )
+    select_background["income_hh_cut"] = pd.cut(
+        select_background["net_income_hh_eqv"],
+        select_background["net_income_hh_eqv"].quantile([0, 0.2, 0.4, 0.6, 0.8, 1]),
+        right=False,
+    )
+    select_background["net_income_hh_eqv"] = (
+        select_background["net_income_hh_eqv"].clip(
+            upper=select_background["net_income_hh_eqv"].quantile(0.99)
+        )
+        / 1000
+    )
+    select_background["net_income_hh"] = (
+        select_background["net_income_hh"].clip(
+            upper=select_background["net_income_hh"].quantile(0.99)
+        )
+        / 1000
+    )
+    select_background["gross_income_hh"] = (
+        select_background["gross_income_hh"].clip(
+            upper=select_background["gross_income_hh"].quantile(0.99)
+        )
+        / 1000
+    )
+    select_background["male"] = 1 - select_background["female"]
 
     select_background.to_pickle(produces)
 
@@ -46,12 +84,16 @@ def task_clean_infected_data(depends_on, produces):
 
     covid_data_list = [pd.read_pickle(wave) for wave in depends_on.values()]
     covid_data = pd.concat(covid_data_list)
-    select_covid = covid_data[["infection_diagnosed","infection_perceived"]].dropna(axis=0, how='all')
-    select_covid['infection_diagnosed'] = select_covid['infection_diagnosed'].cat.rename_categories({'yes, I have been diagnosed with it':'yes'})
+    select_covid = covid_data[["infection_diagnosed", "infection_perceived"]].dropna(
+        axis=0, how="all"
+    )
+    select_covid["infection_diagnosed"] = select_covid[
+        "infection_diagnosed"
+    ].cat.rename_categories({"yes, I have been diagnosed with it": "yes"})
     # 1: infection diagnosed, 0: not infected diagnosed or unsure
-    select_covid['infected'] = select_covid["infection_diagnosed"].eq('yes').astype(int)
+    select_covid["infected"] = select_covid["infection_diagnosed"].eq("yes").astype(int)
 
-    select_covid['Month'] = select_covid.index.get_level_values('month').month_name()
+    select_covid["Month"] = select_covid.index.get_level_values("month").month_name()
     select_covid.to_pickle(produces)
 
 
@@ -66,21 +108,42 @@ def task_clean_compliance_data(depends_on, produces):
 
     covid_data_list = [pd.read_pickle(wave) for wave in depends_on.values()]
     covid_data = pd.concat(covid_data_list)
-    compliance = covid_data[[
-        'avoid_busy_places', 'avoid_public_places', 'maintain_distance', 'adjust_school_work', 'quarantine_symptoms',
-        'quarantine_no_symptoms', 'no_avoidance_behaviors', 'comply_curfew_self'
-    ]].dropna(axis=0, how='any')
+    compliance = covid_data[
+        [
+            "avoid_busy_places",
+            "avoid_public_places",
+            "maintain_distance",
+            "adjust_school_work",
+            "quarantine_symptoms",
+            "quarantine_no_symptoms",
+            "no_avoidance_behaviors",
+            "comply_curfew_self",
+        ]
+    ].dropna(axis=0, how="any")
     # compliance_index is sum of these compliance variables
-    compliance['compliance_index'] = 0
-    loc = (compliance['no_avoidance_behaviors'] != 1) & (compliance['comply_curfew_self'] != "no")
-    compliance.loc[loc, 'compliance_index'] = compliance.loc[
-        loc,
-        ['avoid_busy_places', 'avoid_public_places', 'maintain_distance', 'adjust_school_work', 'quarantine_symptoms',
-         'quarantine_no_symptoms']
-    ].sum(axis='columns').astype(int)
+    compliance["compliance_index"] = 0
+    loc = (compliance["no_avoidance_behaviors"] != 1) & (
+        compliance["comply_curfew_self"] != "no"
+    )
+    compliance.loc[loc, "compliance_index"] = (
+        compliance.loc[
+            loc,
+            [
+                "avoid_busy_places",
+                "avoid_public_places",
+                "maintain_distance",
+                "adjust_school_work",
+                "quarantine_symptoms",
+                "quarantine_no_symptoms",
+            ],
+        ]
+        .sum(axis="columns")
+        .astype(int)
+    )
 
-    compliance['Month'] = compliance.index.get_level_values('month').month_name()
+    compliance["Month"] = compliance.index.get_level_values("month").month_name()
     compliance.to_pickle(produces)
+
 
 @pytask.mark.depends_on(
     {
@@ -92,21 +155,29 @@ def task_clean_compliance_data(depends_on, produces):
 def task_clean_work_status_data(depends_on, produces):
 
     covid_data_list = [pd.read_pickle(wave) for wave in depends_on.values()]
-    covid_data = pd.concat(covid_data_list).reset_index('month')
-    month = covid_data['month'].replace(pd.Timestamp(year=2020, month=2, day=1),
-                                                     pd.Timestamp(year=2020, month=4, day=1))
-    covid_data['month'] = month
-    select_covid = covid_data.set_index(['month'],append=True)[["work_status"]].dropna(axis=0, how='any')
+    covid_data = pd.concat(covid_data_list).reset_index("month")
+    month = covid_data["month"].replace(
+        pd.Timestamp(year=2020, month=2, day=1), pd.Timestamp(year=2020, month=4, day=1)
+    )
+    covid_data["month"] = month
+    select_covid = covid_data.set_index(["month"], append=True)[["work_status"]].dropna(
+        axis=0, how="any"
+    )
 
-    select_covid.loc[:, 'occupation'] = pd.Categorical(select_covid['work_status'].replace({
-        "retired": "unemployed",
-        "homemaker": "unemployed",
-        "student or trainee": "unemployed",
-        "social assistance": "unemployed",
-        "self-employed": "employed"
-    })).reorder_categories(['unemployed','employed'])
+    select_covid.loc[:, "occupation"] = pd.Categorical(
+        select_covid["work_status"].replace(
+            {
+                "retired": "unemployed",
+                "homemaker": "unemployed",
+                "student or trainee": "unemployed",
+                "social assistance": "unemployed",
+                "self-employed": "employed",
+            }
+        )
+    ).reorder_categories(["unemployed", "employed"])
 
     select_covid.to_pickle(produces)
+
 
 @pytask.mark.depends_on(
     {
@@ -119,7 +190,7 @@ def task_clean_essential_worker_data(depends_on, produces):
 
     covid_data_list = [pd.read_pickle(wave) for wave in depends_on.values()]
     covid_data = pd.concat(covid_data_list)
-    select_covid = covid_data[["essential_worker"]].dropna(axis=0, how='any')
+    select_covid = covid_data[["essential_worker"]].dropna(axis=0, how="any")
 
-    select_covid['Month'] = select_covid.index.get_level_values('month').month_name()
+    select_covid["Month"] = select_covid.index.get_level_values("month").month_name()
     select_covid.to_pickle(produces)
