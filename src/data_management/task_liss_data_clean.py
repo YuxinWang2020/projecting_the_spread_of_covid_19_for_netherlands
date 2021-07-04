59f1b884235946df40c2463e63629e2fc1986c0d
YuxinWang2020 <yuxinwang9712@gmail.com>
2021-07-04 18:21:26 +0800

    try to add weight to compliance_index; try to add compliance_index_hh

--- src/data_management/task_liss_data_clean.py
+++ src/data_management/task_liss_data_clean.py
@@ -19,6 +19,7 @@ def task_clean_background_data(depends_on, produces):
             "age",
             "female",
             "edu",
+            "edu_4",
             "net_income",
             "hh_members",
             "hh_children",
@@ -29,7 +30,10 @@ def task_clean_background_data(depends_on, produces):
         ]
     ].dropna(axis=0, how="any")
     select_background["age_cut"] = pd.cut(
-        select_background.age, [15, 25, 50, 75, 120], right=False
+        select_background.age,
+        [15, 25, 50, 75, 120],
+        right=False,
+        labels=["<25", "25-50", "50-75", ">75"],
     )
     select_background["living_alone"] = (select_background["hh_members"] == 1).astype(
         int
@@ -39,7 +43,7 @@ def task_clean_background_data(depends_on, produces):
     ).astype(
         int
     )  # 1: has children, 0: no children
-    select_background["age"] = select_background["age"].astype("int")
+    select_background["age"] = select_background["age"].astype("int") / 100
     select_background["hh_id"] = select_background["hh_id"].astype("int")
     select_background["female"] = select_background["female"].astype("int")
     select_background["log_income_hh"] = np.log(
@@ -49,6 +53,7 @@ def task_clean_background_data(depends_on, produces):
         select_background["net_income_hh_eqv"],
         select_background["net_income_hh_eqv"].quantile([0, 0.2, 0.4, 0.6, 0.8, 1]),
         right=False,
+        labels=["lowest", "low", "middle", "high", "highest"],
     )
     select_background["net_income_hh_eqv"] = (
         select_background["net_income_hh_eqv"].clip(
@@ -69,6 +74,16 @@ def task_clean_background_data(depends_on, produces):
         / 1000
     )
     select_background["male"] = 1 - select_background["female"]
+    select_background["edu"].cat.rename_categories(
+        {
+            "lower_secondary_and_lower": "lower sec. & less",
+            "upper_secondary": "upper sec.",
+            "tertiary": "tertiary",
+        },
+        inplace=True,
+    )
+
+    select_background.rename(columns={})
 
     select_background.to_pickle(produces)
 
@@ -99,15 +114,14 @@ def task_clean_infected_data(depends_on, produces):
 
 @pytask.mark.depends_on(
     {
-        month: SRC / "original_data" / "liss" / f"covid_data_{month}.pickle"
-        for month in ["2020_03"]
+        "covid_data": SRC / "original_data" / "liss" / "covid_data_2020_03.pickle",
+        "background": BLD / "data" / "liss" / "background.pickle",
     }
 )
 @pytask.mark.produces(BLD / "data" / "liss" / "compliance.pickle")
 def task_clean_compliance_data(depends_on, produces):
 
-    covid_data_list = [pd.read_pickle(wave) for wave in depends_on.values()]
-    covid_data = pd.concat(covid_data_list)
+    covid_data = pd.read_pickle(depends_on["covid_data"])
     compliance = covid_data[
         [
             "avoid_busy_places",
@@ -137,12 +151,48 @@ def task_clean_compliance_data(depends_on, produces):
                 "quarantine_no_symptoms",
             ],
         ]
-        .sum(axis="columns")
-        .astype(int)
+        # .dot(pd.Series({"avoid_busy_places": 1,
+        #                 "avoid_public_places": 1,
+        #                 "maintain_distance": 1,
+        #                 "adjust_school_work": 2,
+        #                 "quarantine_symptoms": 5,
+        #                 "quarantine_no_symptoms": 6}))
+        .sum(axis="columns").astype(int)
     )
 
     compliance["Month"] = compliance.index.get_level_values("month").month_name()
-    compliance.to_pickle(produces)
+
+    # merge background['hh_id']
+    background = pd.read_pickle(depends_on["background"])
+    merge_data = pd.merge(
+        compliance.reset_index("month"),
+        background[["hh_id", "hh_members"]],
+        on="personal_id",
+        how="inner",
+    )
+
+    # compliance_index_hh = sum of compliance_index in same household/ hh_members
+    compliance_index_hh = (
+        merge_data.groupby("hh_id")
+        .sum()["compliance_index"]
+        .rename("compliance_index_hh")
+    )
+    merge_data = merge_data.join(compliance_index_hh, on="hh_id", how="inner")
+    merge_data["compliance_index_hh"] = (
+        merge_data["compliance_index_hh"] / merge_data["hh_members"]
+    )
+
+    # # compliance_index_hh = min of compliance_index in same household
+    # compliance_index_hh = (
+    #     merge_data.groupby("hh_id")
+    #     .min()["compliance_index"]
+    #     .rename("compliance_index_hh")
+    # )
+    # merge_data = merge_data.join(compliance_index_hh, on="hh_id", how="inner")
+
+    merge_data.set_index("month", append=True, inplace=True)
+    merge_data.drop(columns=["hh_id", "hh_members"], inplace=True)
+    merge_data.to_pickle(produces)
 
 
 @pytask.mark.depends_on(
@@ -172,9 +222,13 @@ def task_clean_work_status_data(depends_on, produces):
                 "student or trainee": "unemployed",
                 "social assistance": "unemployed",
                 "self-employed": "employed",
+                "unemployed": "unemployed",
+                "employed": "employed",
             }
         )
     ).reorder_categories(["unemployed", "employed"])
+    select_covid["employed"] = select_covid["occupation"].eq("employed").astype(int)
+    select_covid["Month"] = select_covid.index.get_level_values("month").month_name()
 
     select_covid.to_pickle(produces)
 
@@ -194,3 +248,24 @@ def task_clean_essential_worker_data(depends_on, produces):
 
     select_covid["Month"] = select_covid.index.get_level_values("month").month_name()
     select_covid.to_pickle(produces)
+
+
+@pytask.mark.depends_on(SRC / "original_data" / "liss" / "cw19l_EN_3.0p.dta")
+@pytask.mark.produces(BLD / "data" / "liss" / "industry.pickle")
+def task_clean_industry_data(depends_on, produces):
+
+    work_schooling = pd.read_stata(depends_on)
+    industry_data = work_schooling[["nomem_encr", "cw19l_m", "cw19l402"]]
+    industry_data.insert(
+        0,
+        "month",
+        pd.to_datetime(industry_data["cw19l_m"], format="%Y%m", errors="ignore"),
+    )
+    industry_data.insert(0, "personal_id", industry_data["nomem_encr"].astype(int))
+    industry_data = (
+        industry_data.set_index(["personal_id", "month"])["cw19l402"]
+        .rename("industry")
+        .dropna()
+    )
+
+    industry_data.to_pickle(produces)
